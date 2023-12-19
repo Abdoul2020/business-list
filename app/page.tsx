46169e33a477ac5276e2673dd2ffa9ab605180f7
AbdoulGraf <131168943+AbdoulGraf@users.noreply.github.com>
2023-12-19 22:05:31 +0300

    businesses

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,6 @@
 import ListPanel from "./components/ListPanels";
 import Header from "@/app/components/layout/Header";
-import { createSupabaseForServerComponent } from "@/lib/supabase.server";
-
-
-
-// const businessData: Database = {
-//   businessList: [],
-// };
-
-export async function getData() {
-  try {
-    const supabase = createSupabaseForServerComponent();
-
-    const { data: businesses, error } = await supabase
-      .from('businesses')
-      .select('*')
-      .order('name', { ascending: true });
-
-    if (error) {
-      console.error("Error fetching businesses:", error.message);
-      return { businesses: [], error: error.message };
-    }
-
-    return { businesses: businesses || [], error: null };
-  } catch (error: any) {
-    console.error("Error in loader:", error);
-    return { businesses: [], error: error.message || "An error occurred" };
-  }
-}
+import { getallBusiness } from "@/lib/getallbusiness";
 
 
 
@@ -35,7 +8,7 @@ export async function getData() {
 export default async function Page() {
 
 
-  const data = await getData();
+  const data = await getallBusiness();
 
 
 
---
+++ lib/getallbusiness.ts
@@ -0,0 +1,27 @@
+import { createSupabaseForServerComponent } from "@/lib/supabase.server";
+
+
+
+
+export async function getallBusiness() {
+
+    
+    try {
+      const supabase = createSupabaseForServerComponent();
+  
+      const { data: businesses, error } = await supabase
+        .from('businesses')
+        .select('*')
+        .order('name', { ascending: true });
+  
+      if (error) {
+        console.error("Error fetching businesses:", error.message);
+        return { businesses: [], error: error.message };
+      }
+  
+      return { businesses: businesses || [], error: null };
+    } catch (error: any) {
+      console.error("Error in loader:", error);
+      return { businesses: [], error: error.message || "An error occurred" };
+    }
+  }
