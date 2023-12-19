dcc54d14698cff1fe552ab8e278bdfb83977ab5f
AbdoulGraf <131168943+AbdoulGraf@users.noreply.github.com>
2023-12-19 20:48:17 +0300

    business

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -1,33 +1,33 @@
 
-import { createSupabaseForRouteHandler } from "@/lib/supabase.server";
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { createSupabaseForRouteHandler } from "@/lib/supabase.server";
+// import { NextApiRequest, NextApiResponse } from 'next';
 
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+// export async function POST(req: NextApiRequest, res: NextApiResponse) {
     
-    const supabase = createSupabaseForRouteHandler();
+//     const supabase = createSupabaseForRouteHandler();
 
-    try{
+//     try{
 
-        const { data: { session } } = await supabase.auth.getSession();
+//         const { data: { session } } = await supabase.auth.getSession();
 
-        if (session) {
-            await supabase.auth.signOut();
-        }
+//         if (session) {
+//             await supabase.auth.signOut();
+//         }
     
     
-        res.status(302).redirect('/');
+//         res.status(302).redirect('/');
 
 
-    } catch (error){
+//     } catch (error){
 
-        console.error('Error signing out:', error);
-        // Handle error, maybe send a different response or status code
-        res.status(500).send('Internal Server Error');
+//         console.error('Error signing out:', error);
+//         // Handle error, maybe send a different response or status code
+//         res.status(500).send('Internal Server Error');
 
 
-    }
+//     }
 
   
 
-}
+// }
